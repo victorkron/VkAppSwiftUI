b3434a65f0eb322bbefbcb7e7c8fa04f097b0c57
Karim <k.kron2015@yandex.ru>
2022-06-24 08:35:41 +0300

    add grid for photo

--- VKWithSwiftUI/Screens/Photos/PhotosView.swift
+++ VKWithSwiftUI/Screens/Photos/PhotosView.swift
@@ -9,26 +9,51 @@ import SwiftUI
 import ASCollectionView
 
 struct PhotosView: View {
+    
+    private let rows: [GridItem]
+    private var size: CGFloat = 80
+    
     @State var photos = [
+        Photo(imageName: "friend"),
+        Photo(imageName: "friend"),
+        Photo(imageName: "friend"),
+        Photo(imageName: "friend"),
         Photo(imageName: "friend"),
         Photo(imageName: "friend"),
         Photo(imageName: "friend"),
         Photo(imageName: "friend")
     ]
     
+    init() {
+        rows = [
+            GridItem(.fixed(self.size)),
+            GridItem(.fixed(self.size)),
+            GridItem(.fixed(self.size))
+        ]
+    }
+    
     var body: some View {
-        
-        List(photos) { photo in
-            Image(photo.imageName) // заглушка прототипа будующей фотографии
-                .resizable()
-                .frame(width: 80, height: 80)
-                .cornerRadius(80 / 2)
-                .clipped()
-                .setShadow(shadowColor: .cyan, shadowRadius: 10.0)
+        GeometryReader { geometry in
+            ScrollView(.horizontal) {
+                LazyHGrid(rows: rows) {
+                    ForEach(0..<photos.count, id: \.self) { index in
+                        Image(photos[index].imageName) // заглушка прототипа будующей фотографии
+                            .resizable()
+                            .frame(width: size, height: size)
+                            .cornerRadius(size / 2)
+                            .clipped()
+                            .setShadow(shadowColor: .cyan, shadowRadius: 10.0)
+                            .padding(10)
+                    }
+                }.frame(
+                    width: geometry.size.width,
+                    height: geometry.size.height
+                )
+            }
+            
         }
-        .navigationTitle("Photos")
-        
-        
+    }
+
         
 //        ASCollectionView(data: photos) { photo, context in
 //            AvatarOfCell {
@@ -42,8 +67,8 @@ struct PhotosView: View {
 //            )
 //
 //        }
-    }
 }
+
 
 struct PhotosView_Previews: PreviewProvider {
     static var previews: some View {
